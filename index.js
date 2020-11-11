970edd68f3089df844441a3a8e103f6ca5bc7a10
Owen Lee <owlee0814@gmail.com>
2020-11-11 02:43:22 -0500

    added volume bar

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ window.addEventListener("keydown", playNote);
 var i = 0;
 var arr = ["tea", "community", "awareness", "economics", "nation", "throat", "dirt", "farmer", "recipe", "currency", "disk", "marketing", "woman", "effort", "moment"];
 var description = "Find your favorite switches before building one. Recommended to wear a headset";
+var volumeBar = document.getElementById("volumeBar");
+var currentVolume = 0.5;
 
 typeWriter();
 
 function playNote(e) {
   	if(e.repeat) return;
-
 	playAudio('sounds/' + `${e.code}` + '.mp3');
+}
 
+volumeBar.oninput = function() {
+  setVolume(this.value);
+}
+
+ function setVolume(newVolume) {
+ 	newVolume = newVolume / 100
+    currentVolume = parseFloat(newVolume);
 }
 
 function playAudio(url) {
-  new Audio(url).play();
+	var audio = new Audio(url);
+	audio.volume = currentVolume;
+  	audio.play();
 }
 
 function typeWriter() {
